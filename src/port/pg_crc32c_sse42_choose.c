6ecf1ef206350207219e62ad26309e3cd004f239
John Naylor <john.naylor@postgresql.org>
2025-03-11 16:55:13 +0700

    Fix unused function warning on CI

--- src/port/pg_crc32c_sse42_choose.c
+++ src/port/pg_crc32c_sse42_choose.c
@@ -81,10 +81,10 @@ pg_comp_crc32c_choose(pg_crc32c crc, const void *data, size_t len)
 	{
 		pg_comp_crc32c = pg_comp_crc32c_sse42;
 
-#ifdef USE_AVX512_CRC32C_WITH_RUNTIME_CHECK
 		if (exx[2] & (1 << 27) &&	/* OSXSAVE */
 			zmm_regs_available())
 		{
+#ifdef USE_AVX512_CRC32C_WITH_RUNTIME_CHECK
 			/* second cpuid call on leaf 7 to check extended avx512 support */
 
 			memset(exx, 0, 4 * sizeof(exx[0]));
@@ -93,15 +93,12 @@ pg_comp_crc32c_choose(pg_crc32c crc, const void *data, size_t len)
 			__get_cpuid_count(7, 0, &exx[0], &exx[1], &exx[2], &exx[3]);
 #elif defined(HAVE__CPUIDEX)
 			__cpuidex(exx, 7, 0);
-#else
-#error cpuid instruction not available
 #endif
 			if (exx[2] & (1 << 10) &&	/* VPCLMULQDQ */
 				exx[1] & (1 << 31)) /* AVX512-VL */
 				pg_comp_crc32c = pg_comp_crc32c_pclmul;
-
-		}
 #endif
+		}
 	}
 
 	return pg_comp_crc32c(crc, data, len);
